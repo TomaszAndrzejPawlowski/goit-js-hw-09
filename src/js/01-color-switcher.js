11d98afb44abb14bf99b020506b55e9c1a6cefa4
Tomasz <pawlowski.t.93@gmail.com>
2023-09-07 19:50:37 +0200

    Finished first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const websiteBody = document.querySelector('body');
+let timerId;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+stopBtn.disabled = true;
+startBtn.addEventListener('click', () => {
+  websiteBody.style.backgroundColor = `${getRandomHexColor()}`;
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  timerId = setInterval(() => {
+    websiteBody.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+});
